7aa544659f8538a99670c4767c8d0996ba474820
Jyrone Parker <jyrone.parker@mgmail.com>
2021-12-15 04:56:59 -0500

    working on data

--- programs/solottery/src/lib.rs
+++ programs/solottery/src/lib.rs
@@ -6,9 +6,37 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod solottery {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.ticket_count = 0;
+
+        Ok(())
+    }
+
+    pub fn buy_ticket(ctx: Context<Increment>, data: u64) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.accounts[base_account.ticket_count] = data;
+        base_account.ticket_count += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer=user, space= 8 + 8)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub ticket_count: u64,
+    pub accounts: [u64; 100]
+}
